e85f2afb397ddb75cdb627dee5f88dd05b3d7273
luoxiangyong <36727308@qq.com>
2017-07-26 14:02:44 +0800

    add ch11

---
+++ programming-ruby/ch11/test-thread.rb
@@ -0,0 +1,16 @@
+#! /usr/bin/ruby -w
+
+
+count = 0
+threads = []
+
+10.times do |i|
+  threads << Thread.new(i) do
+    sleep(rand(0.1))
+    Thread.current['mycount'] = i
+    count += 1
+  end
+end
+
+threads.each { |t| t.join(); print t['mycount'], ' ,'}
+puts "count = #{count}"
